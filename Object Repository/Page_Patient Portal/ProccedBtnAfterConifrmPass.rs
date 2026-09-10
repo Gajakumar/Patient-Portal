3c0a25b1725e54115302c93a056bd6c2a112f16a
Gajakumar <gajakumara@first-insight.com>
2026-09-10 18:03:06 +0530

    10 sept

--- Object Repository/Page_Patient Portal/ProccedBtnAfterConifrmPass.rs
+++ Object Repository/Page_Patient Portal/ProccedBtnAfterConifrmPass.rs
@@ -7,11 +7,10 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value></value>
       </entry>
       <entry>
          <key>XPATH</key>
-         <value>//div[contains(text(),'Proceed')]</value>
+         <value>//div[contains(text(),'Change Password')]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
